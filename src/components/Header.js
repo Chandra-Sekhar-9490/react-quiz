e9dd38b499fcb65497e2f9839a4c65cfb24a2948
Chandra-Sekhar-9490 <schandrasekhar9490@gmail.com>
2024-10-21 17:59:43 +0530

    Initial Header Created

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img src="/images/logo192.png" alt="react-logo" />
+      <h1>React Quiz</h1>
+    </header>
+  );
+}
+export default Header;
