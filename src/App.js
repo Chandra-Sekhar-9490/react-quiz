8b6d897e6d852ee33340dfa7f2c0d42087404654
Chandra-Sekhar-9490 <schandrasekhar9490@gmail.com>
2024-10-22 18:34:46 +0530

    Updated the button to move next question

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./components/Main";
 import { useEffect, useReducer } from "react";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 const initialState = {
   questions: [],
@@ -40,6 +41,13 @@ function reducer(state, action) {
             : state.points,
       };
 
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+
     default:
       throw new Error("Action unknown");
   }
@@ -67,11 +75,14 @@ function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
